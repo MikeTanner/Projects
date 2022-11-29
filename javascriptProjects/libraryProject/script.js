d4e54a98c41f31a204ba7eb7f10a1bec5fa0b26c
Mike Tanner <miketannercs@gmail.com>
2022-11-28 22:42:21 -0600

    Change status button

--- javascriptProjects/libraryProject/script.js
+++ javascriptProjects/libraryProject/script.js
@@ -2,6 +2,16 @@ const container = document.querySelector(".container")
 const displayContainer = document.querySelector(".displayContainer")
 const showBtn = document.querySelector(".showButton")
 const formsetContainer = document.querySelector("#userInput")
+const inputBtn = document.querySelector("#inputBtn")
+
+inputBtn.addEventListener("click",(event)=> {
+    event.preventDefault(); //stops form submitting
+    const addBook = new Book(document.getElementById("inputTitle").value, document.getElementById("inputAuthor").value, document.getElementById("inputDate").value, document.getElementById("inputRead").value);
+    console.log(addBook);
+    addBookToLibrary(addBook);
+
+    
+})
 
 showBtn.addEventListener("click", () => {
     console.log(this);
@@ -16,14 +26,18 @@ showBtn.addEventListener("click", () => {
     }
 
 })
-function Book(title, author, date) {
+
+function Book(title, author, date, readStatus) {
     //constructor
+
     this.title = title;
     this.author = author;
     this.date = date;
+    this.readStatus = readStatus;
 }
-function addBookToLibrary() {
-    //user input
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+    displayBooks();
 }
 
 function displayBooks() {
@@ -33,21 +47,54 @@ function displayBooks() {
     });
         
 }
+function readBtnEvent(event) {
+/*     console.log("break");
+    console.log(event);
+    console.log(event.target.id); */
+    
+    myLibrary.forEach(book => {
+        console.log(book.title);
+        if (event.target.id == book.title) {
+            if (book.readStatus == "Unread") {
+                book.readStatus = "Read"
+            }
+            else {
+                book.readStatus = "Unread"
+            }
+        }
+    
+    });
+    displayBooks();
+    event.stopPropagation();
+}
 function createCard(book) {
     const newCard = document.createElement("div");
     const titleContainer = document.createElement("h2");
     const authorContainer = document.createElement("p");
     const dateContainer = document.createElement("p");
+    const readStatus = document.createElement("p");
+    const readBtn = document.createElement("button");
+    const removeBtn = document.createElement("button");
+
+    document.forms[0].reset();
     newCard.classList.add("card");
     titleContainer.textContent = book.title;
     authorContainer.textContent = book.author;
     dateContainer.textContent = book.date;
-    newCard.append(titleContainer, authorContainer, dateContainer);
+    readStatus.textContent = book.readStatus;
+    readBtn.textContent = "Change Status";
+    readBtn.id = book.title;
+    removeBtn.textContent = "Remove Book";
+    readBtn.addEventListener("click", readBtnEvent);
+/*     removeBtn.addEventListener("click", removeBtnEvent());
+ */    newCard.append(titleContainer, authorContainer, dateContainer, readStatus, readBtn, removeBtn);
     displayContainer.appendChild(newCard);
-}
 
-const bookOne = new Book("Gregology", "Greg Stevens","1945");
-const bookTwo = new Book("Stevenology", "Steven Gregg", "1944");
-const bookThree = new Book("Runebook", "Runescape", "1990");
+/*     addRemoveButtonListener(removeBtn);
+ */}
+
+const bookOne = new Book("Gregology", "Greg Stevens","1945", "Unread");
+const bookTwo = new Book("Stevenology", "Steven Gregg", "1944", "Unread");
+const bookThree = new Book("Runebook", "Runescape", "1990", "Unread");
 let myLibrary = [bookOne, bookTwo, bookThree];
 displayBooks();
